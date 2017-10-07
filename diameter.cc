9b61c09ca7c987298e2fdf513c32f34dd6068a63
Armando Belardo <armandoubelardo@gmail.com>
2017-10-07 09:57:05 -0400

    papers code now works, initial tests do not show it's speed

--- diameter.cc
+++ diameter.cc
@@ -1,8 +1,9 @@
+#include <algorithm>
 #include <cstdlib>
 #include <deque>
-#include <vector>
-#include <algorithm>
+#include <stack>
 #include <sys/time.h>
+#include <vector>
 #include "diameter.h"
 
 #define MAX(a,b) a > b? a : b
@@ -35,14 +36,250 @@ namespace {
   }
 
   vector<vector<int> > Transpose(const vector <vector<int> > &adjlist) {
-    return adjlist;
+    vector< vector<int> > transposed(adjlist.size());
+    for (int i = 0; i < adjlist.size(); i++) {
+      for (int neighbor : adjlist[i]) {
+        transposed[neighbor].push_back(i);
+      }
+    }
+    return transposed;
+  }
+
+  int GetRandom(int V) {
+      static unsigned long long x = 123456789;
+      static unsigned long long y = 362436039;
+      static unsigned long long z = 521288629;
+      static unsigned long long w = 88675123;
+      unsigned long long t;
+
+      t = x ^ (x << 11);
+      x = y;
+      y = z;
+      z = w;
+      w = (w ^ (w >> 19)) ^ (t ^ (t >> 8));
+
+      return  w % V;
   }
 } // end namespace
 
 namespace Diameter {
+  // Code as from @kawatea on GitHub <3
+  // TODO(iamabel): potentially clean up the below code and format like rest.
   int GetFastDiam(const vector <vector<int> > &adjlist) {
-    return 1;
+    // Prepare the adjacency list
+    vector <vector <int> > radjlist = Transpose(adjlist);
+    int num_double_sweep = 10, diameter = 0, V = adjlist.size();
+
+    // Decompose the graph into strongly connected components
+    vector <int> scc(V);
+    {
+        int num_visit = 0, num_scc = 0;
+        vector <int> ord(V, -1);
+        vector <int> low(V);
+        vector <bool> in(V, false);
+        stack <int> s;
+        stack <pair<int, int> > dfs;
+
+        for (int i = 0; i < V; i++) {
+            if (ord[i] != -1) continue;
+
+            dfs.push(make_pair(i, -1));
+
+            while (!dfs.empty()) {
+                int v = dfs.top().first;
+                int index = dfs.top().second;
+
+                dfs.pop();
+
+                if (index == -1) {
+                    ord[v] = low[v] = num_visit++;
+                    s.push(v);
+                    in[v] = true;
+                } else {
+                    low[v] = min(low[v], low[adjlist[v][index]]);
+                }
+
+                for (index++; index < (int)adjlist[v].size(); index++) {
+                    int w = adjlist[v][index];
+
+                    if (ord[w] == -1) {
+                        dfs.push(make_pair(v, index));
+                        dfs.push(make_pair(w, -1));
+                        break;
+                    } else if (in[w] == true) {
+                        low[v] = min(low[v], ord[w]);
+                    }
+                }
+
+                if (index == (int)adjlist[v].size() && low[v] == ord[v]) {
+                    while (true) {
+                        int w = s.top();
+
+                        s.pop();
+                        in[w] = false;
+                        scc[w] = num_scc;
+
+                        if (v == w) break;
+                    }
+
+                    num_scc++;
+                }
+            }
+        }
+    }
+
+    // Compute the diameter lower bound by the double sweep algorithm
+    int qs, qt;
+    vector <int> dist(V, -1);
+    vector <int> queue(V);
+    {
+        for (int i = 0; i < num_double_sweep; i++) {
+            int start = GetRandom(V);
+
+            // forward BFS
+            qs = qt = 0;
+            dist[start] = 0;
+            queue[qt++] = start;
+
+            while (qs < qt) {
+                int v = queue[qs++];
+
+                for (size_t j = 0; j < adjlist[v].size(); j++) {
+                    if (dist[adjlist[v][j]] < 0) {
+                        dist[adjlist[v][j]] = dist[v] + 1;
+                        queue[qt++] = adjlist[v][j];
+                    }
+                }
+            }
+
+            for (int j = 0; j < qt; j++) dist[queue[j]] = -1;
+
+            // barkward BFS
+            start = queue[qt - 1];
+            qs = qt = 0;
+            dist[start] = 0;
+            queue[qt++] = start;
+
+            while (qs < qt) {
+                int v = queue[qs++];
+
+                for (size_t j = 0; j < radjlist[v].size(); j++) {
+                    if (dist[radjlist[v][j]] < 0) {
+                        dist[radjlist[v][j]] = dist[v] + 1;
+                        queue[qt++] = radjlist[v][j];
+                    }
+                }
+            }
+
+            diameter = max(diameter, dist[queue[qt - 1]]);
+
+            for (int j = 0; j < qt; j++) dist[queue[j]] = -1;
+        }
+    }
+
+    // Order vertices
+    vector <pair<long long, int> > order(V);
+    {
+        for (int v = 0; v < V; v++) {
+            int in = 0, out = 0;
+
+            for (size_t i = 0; i < radjlist[v].size(); i++) {
+                if (scc[radjlist[v][i]] == scc[v]) in++;
+            }
+
+            for (size_t i = 0; i < adjlist[v].size(); i++) {
+                if (scc[adjlist[v][i]] == scc[v]) out++;
+            }
+
+            // SCC : reverse topological order
+            // inside an SCC : decreasing order of the product of the indegree and outdegree for vertices in the same SCC
+            order[v] = make_pair(((long long)scc[v] << 32) - in * out, v);
+        }
+
+        sort(order.begin(), order.end());
+    }
+
+    // Examine every vertex
+    vector <int> ecc(V, V);
+    {
+        for (int i = 0; i < V; i++) {
+            int u = order[i].second;
+
+            if (ecc[u] <= diameter) continue;
+
+            // Refine the eccentricity upper bound
+            int ub = 0;
+            vector <pair<int, int> > neighbors;
+
+            for (size_t j = 0; j < adjlist[u].size(); j++) neighbors.push_back(make_pair(scc[adjlist[u][j]], ecc[adjlist[u][j]] + 1));
+
+            sort(neighbors.begin(), neighbors.end());
+
+            for (size_t j = 0; j < neighbors.size(); ) {
+                int component = neighbors[j].first;
+                int lb = V;
+
+                for (; j < neighbors.size(); j++) {
+                    if (neighbors[j].first != component) break;
+                    lb = min(lb, neighbors[j].second);
+                }
+
+                ub = max(ub, lb);
+
+                if (ub > diameter) break;
+            }
+
+            if (ub <= diameter) {
+                ecc[u] = ub;
+                continue;
+            }
+
+            // Conduct a BFS and update bounds
+            qs = qt = 0;
+            dist[u] = 0;
+            queue[qt++] = u;
+
+            while (qs < qt) {
+                int v = queue[qs++];
+
+                for (size_t j = 0; j < adjlist[v].size(); j++) {
+                    if (dist[adjlist[v][j]] < 0) {
+                        dist[adjlist[v][j]] = dist[v] + 1;
+                        queue[qt++] = adjlist[v][j];
+                    }
+                }
+            }
+
+            ecc[u] = dist[queue[qt - 1]];
+            diameter = max(diameter, ecc[u]);
+
+            for (int j = 0; j < qt; j++) dist[queue[j]] = -1;
+
+            qs = qt = 0;
+            dist[u] = 0;
+            queue[qt++] = u;
+
+            while (qs < qt) {
+                int v = queue[qs++];
+
+                ecc[v] = std::min(ecc[v], dist[v] + ecc[u]);
+
+                for (size_t j = 0; j < radjlist[v].size(); j++) {
+                    // only inside an SCC
+                    if (dist[radjlist[v][j]] < 0 && scc[radjlist[v][j]] == scc[u]) {
+                        dist[radjlist[v][j]] = dist[v] + 1;
+                        queue[qt++] = radjlist[v][j];
+                    }
+                }
+            }
+
+            for (int j = 0; j < qt; j++) dist[queue[j]] = -1;
+        }
+    }
+
+    return diameter;
   }
+
   int GetBruteDiam(const vector <vector<int> > &adjlist) {
     int diameter = 0;
 
@@ -51,6 +288,7 @@ namespace Diameter {
     }
     return diameter;
   }
+
   void PrintGraph(const vector <vector<int> > &adjlist) {
     for (int i = 0; i < adjlist.size(); i++) {
       for (int neighbor : adjlist[i]) {
